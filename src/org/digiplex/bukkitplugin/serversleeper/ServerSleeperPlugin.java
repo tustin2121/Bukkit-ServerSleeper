18cb94a7befc52c8a94a2a40870c47f2d6a7cbda
Tim Pittman <tustin2121@gmail.com>
2012-08-31 21:54:59 -0400

    Tested and made it work
    - Made the EVM attach if Commander is present, and not die if not
    - Works like expected
    - Added 5 minute warning.

--- src/org/digiplex/bukkitplugin/serversleeper/ServerSleeperPlugin.java
+++ src/org/digiplex/bukkitplugin/serversleeper/ServerSleeperPlugin.java
@@ -1,3 +1,6 @@
+package org.digiplex.bukkitplugin.serversleeper;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
@@ -5,14 +8,12 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.digiplex.bukkitplugin.commander.api.CommanderAPI;
 
 
 public class ServerSleeperPlugin extends JavaPlugin {
 	public static final Logger Log = Logger.getLogger("Minecraft");
 	
 	public Configuration config;
-	public boolean evmLoaded = false;
 	
 	public int timeInactive = 0;
 	public int postponeTime = 0;
@@ -27,11 +28,12 @@ public class ServerSleeperPlugin extends JavaPlugin {
 		config.options().copyDefaults(true);
 		this.saveConfig();
 		
+		timeUntilShutdown = config.getInt("timeout", 60);
+		
 		//register the inactivity monitor, initial delay is 10 sec, interval is 60 sec
 		this.getServer().getScheduler().scheduleAsyncRepeatingTask(this, new InactivityMonitor(), 10*20L, 60*20L);
 		
-		if (this.getServer().getPluginManager().isPluginEnabled("Commander")) 
-			loadCommanderEVM();
+		loadCommanderEVM();
 		
 		
 		
@@ -54,13 +56,15 @@ public class ServerSleeperPlugin extends JavaPlugin {
 		@Override public void run() {
 			try {
 				int numPlayers = getServer().getScheduler().callSyncMethod(ssplugin, getPlayers).get();
-				
+				Log.info("[ServerSleeper:InactivityMonitor] Players on server: "+numPlayers);
 				if (numPlayers == 0) {
 					//increase inactivity counters
 					if (postponeTime > 0) postponeTime--;
 					else {
 						timeInactive++;
-						if (timeInactive >= timeUntilShutdown) {
+						if (timeInactive + 5 == timeUntilShutdown) {
+							Log.info("[ServerSleeper] Server will shut down due to inactivity in 5 minutes.");
+						} else if (timeInactive >= timeUntilShutdown) {
 							getServer().getScheduler().scheduleSyncDelayedTask(ssplugin, new Runnable() {
 								//shut down from main server thread
 								@Override public void run() { shutdownServer(); }
@@ -70,7 +74,7 @@ public class ServerSleeperPlugin extends JavaPlugin {
 				} else {
 					timeInactive = 0;
 				}
-				
+				Log.info("[ServerSleeper:InactivityMonitor] tI="+timeInactive+"  pp="+postponeTime+"  tus="+timeUntilShutdown);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} catch (ExecutionException e) {
@@ -85,13 +89,51 @@ public class ServerSleeperPlugin extends JavaPlugin {
 	}
 	
 	//////////////// Commander Hooks ///////////////////
+	private boolean evmLoaded = false;
 	
 	private void loadCommanderEVM() {
-		CommanderAPI.registerEVM(new SleeperEVM(this));
-		evmLoaded = true;
+		if (!this.getServer().getPluginManager().isPluginEnabled("Commander"))
+			return;
+		
+		try {
+			Class<?> c = Class.forName("org.digiplex.bukkitplugin.serversleeper.SleeperEVM");
+			Method m = c.getDeclaredMethod("loadCommanderEVM", ServerSleeperPlugin.class);
+			m.invoke(null, this);
+			evmLoaded = true;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void unloadCommanderEVM() {
-		CommanderAPI.unregisterEVM("ServerSleeper");
+		if (!evmLoaded) return;
+		try {
+			Class<?> c = Class.forName("org.digiplex.bukkitplugin.serversleeper.SleeperEVM");
+			Method m = c.getDeclaredMethod("unloadCommanderEVM");
+			m.invoke(null);
+			evmLoaded = false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/org/digiplex/bukkitplugin/serversleeper/SleeperEVM.java
+++ src/org/digiplex/bukkitplugin/serversleeper/SleeperEVM.java
@@ -1,10 +1,20 @@
+package org.digiplex.bukkitplugin.serversleeper;
 import org.bukkit.command.CommandSender;
 import org.digiplex.bukkitplugin.commander.api.BadEVPathException;
 import org.digiplex.bukkitplugin.commander.api.CmdrEnvVarModule;
+import org.digiplex.bukkitplugin.commander.api.CommanderAPI;
 
 
 public class SleeperEVM implements CmdrEnvVarModule {
 	private ServerSleeperPlugin plugin;
+	
+	public static void loadCommanderEVM(ServerSleeperPlugin plugin) {
+		CommanderAPI.registerEVM(new SleeperEVM(plugin));
+	}
+	
+	public static void unloadCommanderEVM() {
+		CommanderAPI.unregisterEVM("ServerSleeper");
+	}
 	
 	public SleeperEVM(ServerSleeperPlugin plugin) {
 		this.plugin = plugin;
